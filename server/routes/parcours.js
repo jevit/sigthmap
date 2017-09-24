7d5f2d58c9c20189833701f2f6cad71d8b10fad3
jevit <jeremie.viteau@gmail.com>
2017-09-24 09:49:37 +0200

    add user on mongo hk

--- server/routes/parcours.js
+++ server/routes/parcours.js
@@ -78,9 +78,8 @@ exports.update = function(req, res) {
     console.log('Updating parcours: ' + id);
     console.log(JSON.stringify(parcours));
     db.collection('parcours', function(err, collection) {
-		console.log(parcours);
-		collection.update({'_id':id}, parcours, {safe:true}, function(err, result) {
-        //collection.findAndModify({'_id':id}, [['_id','asc']], parcours, {}, function(err, result) {
+    //   collection.update({'_id':new mongo.ObjectID(id)}, parcours, {safe:true}, function(err, result) {
+        collection.findAndModify({'_id':new mongo.ObjectID(id)},[],parcours, {safe:true}, function(err, result) {
             if (err) {
                 console.log('Error updating parcours: ' + err);
                 res.send({'error':'An error has occurred'});
