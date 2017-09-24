c2a7cbfa5defcf24d8cf7be4c377c82d90ebdbba
jevit <jeremie.viteau@gmail.com>
2017-09-24 15:25:07 +0200

    add user on mongo hk

--- server/routes/parcours.js
+++ server/routes/parcours.js
@@ -30,90 +30,92 @@ db.open(function(err, db) {
 	}
 });
 
-/* Retourne un objet */
-exports.findById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving parcours: ' + id);
-    db.collection('parcours', function(err, collection) {
-		console.log(id);
-		collection.find({'_id':new mongo.ObjectID(id)}).limit(1).next(
-		function(err, item){
-            res.send(item);
-        });
-    });
-};
 
-/* Retourne tous les objets */
-exports.findAll = function(req, res) {
-    db.collection('parcours', function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            res.send(items);
-        });
-    });
-};
-
-/* Ajoute une donnée */
-exports.add = function(req, res) {
-    var parcours = req.body;
-	if(parcours._id !== "" && parcours._id !== null ){
-		console.log('Update');
-		this.update(req, res);
-	}
-	else{
-		console.log('ADD');
-		parcours._id = new mongo.ObjectID();
-		console.log('Adding parcours: ' + JSON.stringify(parcours));
+var self = module.exports = {
+	/* Retourne un objet */
+	findById = function(req, res) {
+		var id = req.params.id;
+		console.log('Retrieving parcours: ' + id);
 		db.collection('parcours', function(err, collection) {
-			collection.insert(parcours, {safe:true}, function(err, result) {
+			console.log(id);
+			collection.find({'_id':new mongo.ObjectID(id)}).limit(1).next(
+			function(err, item){
+				res.send(item);
+			});
+		});
+	};
+
+	/* Retourne tous les objets */
+	findAll = function(req, res) {
+		db.collection('parcours', function(err, collection) {
+			collection.find().toArray(function(err, items) {
+				res.send(items);
+			});
+		});
+	};
+
+	/* Ajoute une donnée */
+	add = function(req, res) {
+		var parcours = req.body;
+		if(parcours._id !== "" && parcours._id !== null ){
+			console.log('UPDATE');
+			self.update(req, res);
+		}
+		else{
+			console.log('ADD');
+			parcours._id = new mongo.ObjectID();
+			console.log('Adding parcours: ' + JSON.stringify(parcours));
+			db.collection('parcours', function(err, collection) {
+				collection.insert(parcours, {safe:true}, function(err, result) {
+					if (err) {
+						res.send({'error':'Erreur '+ err.errmsg});
+						console.log(parcours._id);
+					} else {
+						console.log('Success: ' + JSON.stringify(result[0]));
+						res.send(result[0]);
+					}
+				});
+			});
+		}
+	}
+
+	/* Mise à jour des données */
+	update = function(req, res) {
+		var id = req.params.id;
+		var parcours = req.body;
+		console.log(req.body);
+		console.log('Updating parcours: ' + id);
+		console.log(JSON.stringify(parcours));
+		db.collection('parcours', function(err, collection) {
+		   collection.update({'_id':new mongo.ObjectID(id)}, {$set:parcours}, {}, function(err, result) {
+		  //  collection.findAndModify({'_id':new mongo.ObjectID(id)},[],parcours,{upsert: true}, function(err, result) {
 				if (err) {
-					res.send({'error':'Erreur '+ err.errmsg});
-					console.log(parcours._id);
+					console.log('Error updating parcours: ' + err);
+					res.send({'error':'An error has occurred'});
 				} else {
-					console.log('Success: ' + JSON.stringify(result[0]));
-					res.send(result[0]);
+					console.log('' + result + ' document(s) updated');
+					res.send(parcours);
+				}
+			});
+		});
+	}
+
+	/* Supprime une donnée */
+	delete = function(req, res) {
+		var id = req.params.id;
+		console.log('Deleting parcours: ' + id);
+		db.collection('parcours', function(err, collection) {
+			collection.remove({'_id':new mongo.ObjectID(id)}, {safe:true}, function(err, result) {
+				if (err) {
+					res.send({'error':'An error has occurred - ' + err});
+				} else {
+					console.log('' + result + ' document(s) deleted');
+					res.send(req.body);
 				}
 			});
 		});
 	}
 }
-
-/* Mise à jour des données */
-exports.update = function(req, res) {
-    var id = req.params.id;
-    var parcours = req.body;
-	console.log(req.body);
-    console.log('Updating parcours: ' + id);
-    console.log(JSON.stringify(parcours));
-    db.collection('parcours', function(err, collection) {
-       collection.update({'_id':new mongo.ObjectID(id)}, {$set:parcours}, {}, function(err, result) {
-      //  collection.findAndModify({'_id':new mongo.ObjectID(id)},[],parcours,{upsert: true}, function(err, result) {
-            if (err) {
-                console.log('Error updating parcours: ' + err);
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('' + result + ' document(s) updated');
-                res.send(parcours);
-            }
-        });
-    });
-}
-
-/* Supprime une donnée */
-exports.delete = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting parcours: ' + id);
-    db.collection('parcours', function(err, collection) {
-        collection.remove({'_id':new mongo.ObjectID(id)}, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred - ' + err});
-            } else {
-                console.log('' + result + ' document(s) deleted');
-                res.send(req.body);
-            }
-        });
-    });
-}
-
 /* Remplir la BD au démarrage de l'app si aucune données */
 var populateDB = function() {
 
