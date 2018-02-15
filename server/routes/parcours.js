bc290b7a7ba389e0960397913c5da7cf3fccdc66
jevit <jeremie.viteau@gmail.com>
2018-02-15 22:23:08 +0100

    a

--- server/routes/parcours.js
+++ server/routes/parcours.js
@@ -85,6 +85,7 @@ var self = module.exports = {
 		console.log(req.body);
 		console.log('Updating parcours: ' + id);
 		console.log(JSON.stringify(parcours));
+		delete parcours._id;
 		db.collection('parcours', function(err, collection) {
 		   collection.update({'_id':new mongo.ObjectID(id)}, {$set:parcours}, {}, function(err, result) {
 		  //  collection.findAndModify({'_id':new mongo.ObjectID(id)},[],parcours,{upsert: true}, function(err, result) {
